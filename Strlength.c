92f4e73da1931a219b3503d46f6e8ee43c3688f2
Nav1717 <as9554@nyu.edu>
2016-02-05 11:36:38 -0500

    skeleton for stringlength

---
+++ Strlength.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int stringlength(char string []);
+int main()
+{
+	char str[]="The quick brown fox";
+	printf("The length of the string %s is %d\n", str, stringlength(str));
+
+}
+int stringlength(char string[])
+
