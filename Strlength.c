35cca7bf7ac77add261c0eb998a6f504b0166852
Nav1717 <as9554@nyu.edu>
2016-02-09 22:46:28 -0500

    This is the homework

--- Strlength.c
+++ Strlength.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 int stringlength(char string []);
+
 int main()
 {
-	char str[]="The quick brown fox";
+	char str[]=("The quick brown fox");
 	printf("The length of the string %s is %d\n", str, stringlength(str));
 
 }
 int stringlength(char string[])
+{
+	int i;
+
+	for (i=0; string[i]!='\0'; ++i);
+	return i;
+}
+
+
 
---
+++ Temp.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+	int cel;
+	int fhr;
+	printf("_____________________________\n");
+	printf("| Fahrenheit |   Celsius    |\n");
+	printf("|____________|______________|\n");
+		
+	for (fhr=0; fhr<= 300; fhr = fhr+10)
+	{
+		cel = 5 * (fhr - 32)/9;
+		printf ("|%7d     | %7d      | \n",fhr, cel);
+		printf("|____________|______________|\n");
+	}
+	return 0;
+}
